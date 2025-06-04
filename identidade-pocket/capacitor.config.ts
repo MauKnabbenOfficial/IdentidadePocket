685c9a750755417e3e232558a696aa39e4dbba3c
Mauricio Knabben <91981855+MauKnabbenOfficial@users.noreply.github.com>
2025-06-03 21:02:26 -0300

    FirstCommit - Project Vue + Ionic

---
+++ identidade-pocket/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'identidade-pocket',
+  webDir: 'dist'
+};
+
+export default config;
